14411314f37b022f01b41cc344d5aeaec1fe9e7b
Ivan Chen <ivanchen07@gmail.com>
2024-05-22 14:38:24 -0400

    Add main.c + main.h

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+
+    if (strcmp(argv[1], "encrypt") == 0) {
+
+    }
+
+
+    else if (strcmp(argv[1], "decrypt") == 0) {
+
+    }
+
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BLOCK_SIZE 64
+#define KEY_SIZE 56
+
+#endif
