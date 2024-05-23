34b8a0ddb9810557ff01f3df8e66f55c1915a06b
Ivan Chen <ivanchen07@gmail.com>
2024-05-23 18:50:34 -0400

    Make pbin function print in big endian

--- des.c
+++ des.c
@@ -2,12 +2,13 @@
 #include "des.h"
 
 void pbin(uint64_t input) {
-    for (int i = 0; i < 64; i++) {
-        if ((input >> i) & 1) 
-            printf("1");
-        else
-            printf("0");
+    char count = 0;
+    for (int i = 63; i >= 0; i--) {
+        if (count++ % 8 == 0) printf(" ");
+        if ((input >> i) & 1) printf("1");
+        else                  printf("0");
     }
+    printf("\n");
 }
 
 uint64_t initial_permutation(uint64_t input) {
