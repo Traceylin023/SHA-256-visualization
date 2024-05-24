0848cdff33ca4e6a9d8c846f91f9b6e570cecf24
Ivan Chen <ivanchen07@gmail.com>
2024-05-24 11:41:47 -0400

    Add more tests for new functions

--- main.c
+++ main.c
@@ -10,14 +10,21 @@ int main(int argc, char* argv[]) {
     // R: 1864390500
     // KEY: 3759860997584293892
 
-    pbin(5216694956355245935ULL);
-    pbin(initial_permutation(5216694956355245935ULL));
+    pbin(5216694956355245935ULL, 64);
+    pbin(initial_permutation(5216694956355245935ULL), 64);
 
     printf("\n");
 
-    pbin(3759860997584293892ULL);
-    pbin(pc_1_c(3759860997584293892ULL));
-    pbin(pc_1_d(3759860997584293892ULL));
+    pbin(3759860997584293892ULL, 64);
+    pbin(pc_1_c(3759860997584293892ULL), 28);
+    pbin(pc_1_d(3759860997584293892ULL), 28);
+
+    pbin(key_shift(pc_1_c(3759860997584293892ULL), 1), 28);
+    pbin(key_shift(pc_1_d(3759860997584293892ULL), 1), 28);
+
+    pbin(pc_2(key_shift(pc_1_c(3759860997584293892ULL), 1), key_shift(pc_1_d(3759860997584293892ULL), 1)), 48);
+
+    // printf("\n");
 
     // if (strcmp(argv[1], "encrypt") == 0) {
 
